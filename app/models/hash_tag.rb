c06d16b5c82cfdda08291851350b1716f31a3e41
Gaddict <ksano6253@gmail.com>
2015-09-13 21:31:27 +0900

    バリデーションを追加 #24

--- app/models/hash_tag.rb
+++ app/models/hash_tag.rb
@@ -13,6 +13,8 @@ class HashTag < ActiveRecord::Base
   has_many :bot_hash_tag_rels
   has_many :bots, :through => :bot_hash_tag_rels, :dependent => :destroy
 
+  validates :hash_tag, presence: true
+
     # 論理削除されていないレコードを全取得
     def self.find_by_alive
       HashTag.where(deleted: false)
