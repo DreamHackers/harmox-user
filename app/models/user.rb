267f8282eb434a251c8db0563c70525da2e8a1ae
pollseed <cb.so.devope+0001@gmail.com>
2015-09-13 16:15:40 +0900

    ref #26
    er図,モデルを見てschemaを把握したい,SQL文を詳しく開発中に見る

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  email               :string           default(""), not null
+#  encrypted_password  :string           default(""), not null
+#  remember_created_at :datetime
+#  sign_in_count       :integer          default(0), not null
+#  current_sign_in_at  :datetime
+#  last_sign_in_at     :datetime
+#  current_sign_in_ip  :string
+#  last_sign_in_ip     :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  username            :string
+#  provider            :string
+#  uid                 :string
+#
+# Indexes
+#
+#  index_users_on_username  (username) UNIQUE
+#
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- spec/models/bot_hash_tag_rel_spec.rb
+++ spec/models/bot_hash_tag_rel_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: bot_hash_tag_rels
+#
+#  id          :integer          not null, primary key
+#  bot_id      :integer
+#  hash_tag_id :integer
+#  deleted     :boolean          default(FALSE), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe BotHashTagRel, type: :model do
